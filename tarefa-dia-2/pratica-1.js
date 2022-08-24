56077cc751fcf0731f3abb9634f2efe0f1713c99
mateus carvalho <nerfalem123@gmail.com>
2022-08-24 01:30:48 -0300

    liçao-dia-2

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,20 @@
+// nome
+let meuNome
+meuNome = "Mateus"
+// email
+let meuEmail
+meuEmail = "nerfalem123@gmail.com"
+//minha profissao
+let profissão
+profissão = "tosador"
+// minha idade
+let Idade = "32"
+// local que sonho em conhecer
+let Viajar
+Viajar = "Tokyo"
+
+
+
+
+
+ 
