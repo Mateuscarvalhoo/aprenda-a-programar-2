f7815d15ee6521bc0217e9b2884b81467397a487
Mateus Carvalho <nerfalem123@gmail.com>
2022-08-26 00:04:27 -0300

    multiplicador

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,11 @@
+let multiplicador = 5 
+let n = 10
+let i = 1
+
+
+while (i <= n) {
+    let resultado = multiplicador * i;
+    let resulmo = `${multiplicador} x ${i} = ${resultado}`
+    console.log (resulmo)
+    i++
+}
