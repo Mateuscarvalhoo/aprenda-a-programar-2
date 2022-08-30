dfff2740a6ccadce9ea26692ea812ad38e4fb154
Mateus Carvalho <nerfalem123@gmail.com>
2022-08-29 22:47:14 -0300

    site-2

---
+++ projeto-site/script.js
@@ -0,0 +1,22 @@
+const inputCurso = document.getElementById('curso-novo')
+
+function adicionarCurso() {
+    const item = document.createElement('li')
+ 
+
+    item.innerHTML = `<li>
+    <span>${inputCurso.value}</span>
+    <button class="excluir">x</button>
+  </li>`;
+
+    const lista = document.querySelector('#lista')
+    lista.appendChild(item)
+    
+
+
+}
+const botao = document.querySelector('#btn-adicionar')
+botao.addEventListener('click', ()=>console.log('clicou'))
+
+
+
